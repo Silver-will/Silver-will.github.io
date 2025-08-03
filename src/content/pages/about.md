23ecb79953162f71206d38aa8bd9b5b74b33be97
silver-will <99222613+Silver-will@users.noreply.github.com>
2025-08-03 14:47:10 +0100

    Astro portfolio setup

---
+++ src/content/pages/about.md
@@ -0,0 +1,26 @@
+---
+title: About
+seo:
+  title: About Me
+  description: Learn more about the person behind the website and embark on a journey of inspiration and shared experiences.
+  image:
+    src: '/about.jpeg'
+    alt: A person sitting at a desk in front of a computer
+---
+
+![Alt text for image](/about.jpeg)
+
+## Daunte Omoregie - GPU programmer based in Nigeria
+
+**Greetings!** I'm Daunte Omoregie, a passionate web developer residing in the picturesque landscapes of [Nigeria](https://en.wikipedia.org/wiki/Nigeria). With a keen eye for design and a love for crafting seamless digital experiences, I bring a unique blend of creativity and technical expertise to the world of web development.
+
+## Skills and Expertise
+
+I specialize in GPU graphics programming, using vulkan the latest technologies to build responsive and user-friendly websites. My proficiency includes **HTML**, **CSS**, **JavaScript**, and frameworks such as **Astro.js** for dynamic and interactive user interfaces. On the server side, I'm well-versed in Node.js and have experience with database management systems like MongoDB.
+
+
+## Get in Touch
+
+Are you looking to elevate your online presence or bring your digital ideas to life? I'd love to hear from you! Feel free to reach out for collaboration, consultation, or just a friendly chat about all things web development.
+
+_Let's build something amazing together!_
---
+++ src/data/site-config.ts
@@ -0,0 +1,116 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    website: string;
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    website: 'https://silver-will.github.io',
+    title: 'Daunte Omoregie',
+    subtitle: 'Graphics programmer and game engine developer',
+    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'Terms',
+            href: '/terms'
+        }
+    ],
+    socialLinks: [
+        {
+            text: 'Github',
+            href: 'https://github.com/Silver-will'
+        },
+        {
+            text: 'Linkedin',
+            href: 'https://www.linkedin.com/in/daunte-o-4a596622b/'
+        },
+        {
+            text: 'X/Twitter',
+            href: 'https://twitter.com/'
+        }
+    ],
+    hero: {
+        title: 'Hi There & Welcome to My Corner of the Web!',
+        text: "I'm **Daunte omoregie**, a graphics programmer and game developer with a passion for realtime rendering, light tranport and GPU simulation software. Currently a computer engineering undergraduate, i enjoy implementing computer graphics techniques in my free time. Feel free to explore some of my coding endeavors on <a href ='https://github.com/Silver-will'>GitHub</a> or follow me on <a href = 'https://twitter.com/silver_will_'>Twitter/X </a>.",
+
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
